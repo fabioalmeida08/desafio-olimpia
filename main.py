c084e8ad4d0291236329cedcabbb9ad79470345a
fabio <fabioalmeida0896@gmail.com>
2025-12-10 12:06:32 -0300

    refactor: used new clear_term functio to give a more user friendly experience

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from agent.agent import FinanceAgent
-from utils import load_api_key
+from utils.utils import load_api_key, clear_term
 
 if __name__ == "__main__":
     api_key = load_api_key()
 
+    clear_term()
+
     empresa = input("Digite o nome da empresa: ")
+
     if not empresa:
         raise Exception("Digite um nome valido para empresa")
+
     agent = FinanceAgent(api_key, empresa)
     agent.generate_report()
